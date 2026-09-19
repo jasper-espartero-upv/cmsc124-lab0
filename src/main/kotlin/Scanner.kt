6e073d98580b56ff3f1e56acaa70ffb2a52ff101
Jasper Espartero <jsespartero@up.edu.ph>
2026-09-20 00:27:16 +0800

    feat: scan and discard single-line and multi-line comments

--- src/main/kotlin/Scanner.kt
+++ src/main/kotlin/Scanner.kt
@@ -33,7 +33,17 @@ class Scanner(private val source: String) {
             '+' -> addToken(TokenType.PLUS)
             '-' -> addToken(TokenType.MINUS)
             '*' -> addToken(TokenType.STAR)
-            '/' -> addToken(TokenType.SLASH)
+            '/' -> {
+                if (match('/')) {
+                    while (peek() != '\n' && !isAtEnd()) {
+                        advance()
+                    }
+                } else if (match('*')) {
+                    blockComment()
+                } else {
+                    addToken(TokenType.SLASH)
+                }
+            }
 
             '=' -> addToken(TokenType.EQUAL)
             '!' -> addToken(TokenType.BANG)
@@ -138,5 +148,33 @@ class Scanner(private val source: String) {
     private fun reportError(line: Int, message: String) {
         System.err.println("[line $line] Error: $message")
         hadError = true
+    }
+
+    private fun blockComment() {
+        while (!(peek() == '*' && peekNext() == '/') && !isAtEnd()) {
+            if (peek() == '\n') {
+                line++
+            }
+            advance()
+        }
+
+        if (isAtEnd()) {
+            reportError(line, "Unterminated block comment.")
+            return
+        }
+
+        advance()
+        advance()
+    }
+
+    private fun match(expected: Char): Boolean {
+        if (isAtEnd() || source[current] != expected) return false
+        current++
+        return true
+    }
+
+    private fun peekNext(): Char {
+        if (current + 1 >= source.length) return '\u0000'
+        return source[current + 1]
     }
 }
