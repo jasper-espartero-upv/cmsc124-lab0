8eeae858311520ea3fe91d3afde7cc36ab4a8243
cjdelemos <cedelemos@up.edu.ph>
2026-09-21 22:32:18 +0800

    feat: added modes to ./run

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,16 +2,43 @@ import java.io.File
 
 fun main(args: Array<String>) {
     if (args.isEmpty()) {
-        println("Usage: ./run <path>")
+        println("Usage: ./run [--tokenize | --parse] <path>")
         return
     }
 
-    val source = File(args[0]).readText()
+    val mode: String
+    val path: String
+
+    // check if mode is specified
+    if (args[0] == "--tokenize" || args[0] == "--parse") {
+        // check if too many arguments
+        if (args.size < 2) {
+            println("Usage: ./run ${args[0]} <path>")
+            return
+        }
+
+
+        mode = args[0]
+        path = args[1]
+    } else {
+        mode = "default"
+        path = args[0]
+    }
+
+    val source = File(path).readText()
 
     val scanner = Scanner(source)
     val tokens = scanner.scanTokens()
 
-    for (token in tokens) {
-        println(token)
+    when (mode) {
+        "default", "--tokenize" -> {
+            for (token in tokens) {
+                println(token)
+            }
+        }
+
+        "--parse" -> {
+            // Parser goes here later
+        }
     }
 }
